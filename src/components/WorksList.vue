4b45de1836c660ad406ea568d35cdbf2dd7386de
jueon <merryku9@gmail>
2022-02-09 11:39:02 +0900

    220209-v1

--- src/components/WorksList.vue
+++ src/components/WorksList.vue
@@ -5,7 +5,7 @@
                 <figure class="itemBox">
                     <div class="thumb">
                         <!-- <img :src="item.imgSrc" :alt="item.title+' 썸네일 이미지'" onerror="this.src='//new.dev.wivismall.com//image/web/common/item_comingsoon.jpg'"> -->
-                        <img :src="item.imgSrc" :alt="item.title+' 썸네일 이미지'" @error="emptyImage">
+                        <img :src="item.imgSrc" :alt="item.title+' 썸네일 이미지'" ><!-- @error="emptyImage" -->
                     </div>
                     <figcaption>
                         <dl>
@@ -56,11 +56,11 @@ export default {
                 }
             })
         },
-        emptyImage (e) {
+        /* emptyImage (e) {
             console.log(e);
             let imgUrl = 'http://jueon.dothome.co.kr/images/portfolio/noimg.jpg';
             e.target.src = imgUrl;
-        }
+        } */
     },
 }
 </script>
