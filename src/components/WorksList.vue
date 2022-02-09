56323e51b78e1870e4326117bdd5f0f1ff30d830
jueon <merryku9@gmail>
2022-02-09 15:06:53 +0900

    이미지 처리

--- src/components/WorksList.vue
+++ src/components/WorksList.vue
@@ -1,12 +1,11 @@
 <template>
-    npm!
     <ul class="list-gallery">
-        <li :key="id" v-for="(item, id) in options" :class="'item item-'+item.id">
+        <li v-for="(item, id) in options" :key="id" :class="'item item-'+item.id">
             <router-link :to="{name: 'Works', query: {view: 'view', id: item.id}}" v-if="item.show">
                 <figure class="itemBox">
                     <div class="thumb">
-                        <!-- <img :src="item.imgSrc" :alt="item.title+' 썸네일 이미지'" onerror="this.src='//new.dev.wivismall.com//image/web/common/item_comingsoon.jpg'"> -->
-                        <img :src="item.imgSrc" :alt="item.title+' 썸네일 이미지'"><!-- @error="emptyImage" -->
+                        <!-- <img :src="item.imgURL" :alt="item.title+' 썸네일 이미지'" onerror="this.src='//new.dev.wivismall.com//image/web/common/item_comingsoon.jpg'"> -->
+                        <img :src="item.imgURL" :alt="item.title+' 썸네일 이미지'"><!-- @error="emptyImage" -->
                     </div>
                     <figcaption>
                         <dl>
@@ -28,22 +27,22 @@ export default {
         return {
             pageTitle: 'Portfolio',
             options: [
-                {id: 0, title: 'TBH SHOP', workType: '구축', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/1.jpg', show: true},
-                {id: 1, title: '모코블링', workType: '리뉴얼', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/2.jpg', show: true},
-                {id: 2, title: '내츄럴라이프', workType: '운영, 유지보수', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/3.jpg', show: true},
-                {id: 3, title: '돌리버리', workType: '구축', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/4.jpg', show: true},
-                {id: 4, title: '클라라', workType: '구축', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/5.jpg', show: true},
-                {id: 5, title: '인동에프엔 IDF MALL', workType: '구축', version: 'PC', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/7.jpg', show: true},
-                {id: 6, title: '임블리', workType: '리뉴얼', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/8.jpg', show: true},
-                {id: 7, title: '셀렉온', workType: '구축', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/9.jpg', show: true},
-                {id: 8, title: '에바주니', workType: '리뉴얼', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/15.jpg', show: true},
-                {id: 9, title: '호밀', workType: '리뉴얼', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/14.jpg', show: true},
-                {id: 10, title: '좋은사람들 B2C', workType: '구축', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/11.jpg', show: true},
-                {id: 11, title: '남도애꽃', workType: '리뉴얼', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/12.jpg', show: true},
-                {id: 12, title: '쉬비치', workType: '리뉴얼', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/6.jpg', show: true},
-                {id: 13, title: '위드이픈', workType: '리뉴얼', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/16.jpg', show: true},
-                {id: 14, title: '세리박스', workType: '구축', version: 'PC / Mobile', imgSrc: 'http://jueon.dothome.co.kr/images/portfolio/10.jpg', show: false},
-                {id: 99, title: '이미지', workType: '깨지는거', version: '테스트용', imgSrc: '/error_src.jpg', show: true},
+                {id: 0, title: 'TBH SHOP', workType: '구축', version: 'PC / Mobile', imgURL: require('@/assets/images/1.jpg'), show: true},
+                {id: 1, title: '모코블링', workType: '리뉴얼', version: 'PC / Mobile', imgURL: require('@/assets/images/2.jpg'), show: true},
+                {id: 2, title: '내츄럴라이프', workType: '운영, 유지보수', version: 'PC / Mobile', imgURL: require('@/assets/images/3.jpg'), show: true},
+                {id: 3, title: '돌리버리', workType: '구축', version: 'PC / Mobile', imgURL: require('@/assets/images/4.jpg'), show: true},
+                {id: 4, title: '클라라', workType: '구축', version: 'PC / Mobile', imgURL: require('@/assets/images/5.jpg'), show: true},
+                {id: 5, title: '인동에프엔 IDF MALL', workType: '구축', version: 'PC', imgURL: require('@/assets/images/7.jpg'), show: true},
+                {id: 6, title: '임블리', workType: '리뉴얼', version: 'PC / Mobile', imgURL: require('@/assets/images/8.jpg'), show: true},
+                {id: 7, title: '셀렉온', workType: '구축', version: 'PC / Mobile', imgURL: require('@/assets/images/9.jpg'), show: true},
+                {id: 8, title: '에바주니', workType: '리뉴얼', version: 'PC / Mobile', imgURL: require('@/assets/images/15.jpg'), show: true},
+                {id: 9, title: '호밀', workType: '리뉴얼', version: 'PC / Mobile', imgURL: require('@/assets/images/14.jpg'), show: true},
+                {id: 10, title: '좋은사람들 B2C', workType: '구축', version: 'PC / Mobile', imgURL: require('@/assets/images/11.jpg'), show: true},
+                {id: 11, title: '남도애꽃', workType: '리뉴얼', version: 'PC / Mobile', imgURL: require('@/assets/images/12.jpg'), show: true},
+                {id: 12, title: '쉬비치', workType: '리뉴얼', version: 'PC / Mobile', imgURL: require('@/assets/images/6.jpg'), show: true},
+                {id: 13, title: '위드이픈', workType: '리뉴얼', version: 'PC / Mobile', imgURL: require('@/assets/images/16.jpg'), show: true},
+                {id: 14, title: '세리박스', workType: '구축', version: 'PC / Mobile', imgURL: require('@/assets/images/10.jpg'), show: false},
+                //{id: 99, title: '이미지', workType: '깨지는거', version: '테스트용', imgURL: '/error_src.jpg'), show: true},
             ]
         }
     },
@@ -59,7 +58,7 @@ export default {
         },
         /* emptyImage (e) {
             console.log(e);
-            let imgUrl = 'http://jueon.dothome.co.kr/images/portfolio/noimg.jpg';
+            let imgUrl = require(`@/assets/images/noimg.jpg';
             e.target.src = imgUrl;
         } */
     },
